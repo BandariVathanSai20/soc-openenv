0e46060e9af5bc577bbc675ae8b39799cb109fa9
BandariVathanSai20 <vathan809@gmail.com>
2026-04-11 14:29:19 +0530

    chore: update repository with latest files

---
+++ ersvathaOneDriveDesktopsoc-openenv.venvScriptsActivate.ps1
@@ -0,0 +1,18 @@
+[33me06c0bf[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31mhf/main[m[33m)[m Update .dockerignore to optimize Docker build context
+[33ma1758d3[m Update .gitignore to exclude virtual environment folders
+[33ma007178[m Remove virtual environment folder from repository
+[33mcf93919[m Remove unused main.py to align with OpenEnv structure
+[33m47f08a3[m Resolve merge conflicts by keeping updated SOC-OpenEnv implementation
+[33m8f13d66[m Ensure LF line endings for shell scripts
+[33m826ab26[m Initial commit with updated SOC-OpenEnv structure
+[33m7db50ae[m Refactor inference.py to improve error handling
+[33m039ea4f[m Revise README for improved clarity and structure
+[33m3560a04[m Add .dockerignore to exclude unnecessary files
+[33md80739a[m Update package versions in requirements.txt
+[33m5b1468d[m Refactor Dockerfile for improved structure
+[33m8512742[m Update Dockerfile to use Python 3.10 and streamline setup
+[33m07aa550[m Refine scoring and detection logic in grader.py
+[33m9aa1d02[m Update scores
+[33mcb3835c[m Updated project scores
+[33ma872df9[m Fix scoring range (0,1)
+[33m4d2a1d1[m Initial commit - SOC OpenEnv
